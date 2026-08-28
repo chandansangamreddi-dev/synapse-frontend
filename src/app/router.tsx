86923bcfa6f6881fb73cf295a10d8961125881ba
Praveen Beeraka <praveenbeeraka09@gmail.com>
2026-08-29 03:47:36 +0530

    fix: finalize phase 1 routing and styles

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { AppShell } from './AppShell'
 import { LoginPage } from '../features/auth/LoginPage'
-import { OperationsPage } from '../pages/OperationsPage'
 
 export const router = createBrowserRouter([
   {
@@ -11,11 +10,5 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppShell />,
-    children: [
-      {
-        index: true,
-        element: <OperationsPage />,
-      },
-    ],
   },
 ])
