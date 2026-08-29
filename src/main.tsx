0d38e0d2430bc4e7e55788905d59fdb11b653573
Chandan <chandansangamreddi@gmail.com>
2026-08-29 11:09:08 +0530

    fix: mount application router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
 import './index.css'
 import { AppProviders } from './app/AppProviders'
+import { router } from './app/router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppProviders />
+    <AppProviders>
+      <RouterProvider router={router} />
+    </AppProviders>
   </StrictMode>,
 )
